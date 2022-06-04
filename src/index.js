0c3f29e82a65c43ae16698450732a55d2715586c
nomono <nomono.eq@gmail.com>
2022-06-04 12:56:36 +0300

    test store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { rerenderTree } from './render';
 import state from './state/state';
+import App from './App';
+import {
+  addMessage,
+  addPost,
+  updateNewPostText,
+  updateNewMessageText,
+  subscribe,
+} from './state/state';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+export const rerenderTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPost={addPost}
+        addMessage={addMessage}
+        updateNewPostText={updateNewPostText}
+        updateNewMessageText={updateNewMessageText}
+      />
+    </React.StrictMode>
+  );
+};
 rerenderTree(state);
+
+subscribe(rerenderTree);
 
 reportWebVitals();
--- src/state/state.js
+++ src/state/state.js
@@ -1,4 +1,294 @@
-import { rerenderTree } from '../render';
+export let store = {
+  _subscriber() {
+    console.lof('10100101010');
+  },
+
+  _state: {
+    profilePage: {
+      postsData: [
+        {
+          id: 1,
+          name: 'Dima',
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          message: 'Hey,how are you?',
+          likeCounts: 15,
+        },
+        {
+          id: 2,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Alina',
+          message: "Yo, it's my fist post!",
+          likeCounts: 21,
+        },
+        {
+          id: 3,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Ben',
+          message: 'Yes I am!',
+          likeCounts: 12,
+        },
+        {
+          id: 4,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Kim',
+          message: 'Coco Loko! Alo Alo...',
+          likeCounts: 36,
+        },
+      ],
+      newPostText: '',
+    },
+    dialogsPage: {
+      dialogsData: [
+        {
+          id: 1,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Dima',
+        },
+        {
+          id: 2,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Alina',
+        },
+        {
+          id: 3,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Ben',
+        },
+        {
+          id: 4,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Vitaliy',
+        },
+        {
+          id: 5,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Olga',
+        },
+        {
+          id: 6,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Kate',
+        },
+        {
+          id: 7,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Caren',
+        },
+        {
+          id: 8,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Kostya',
+        },
+        {
+          id: 9,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Inna',
+        },
+        {
+          id: 10,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Nick',
+        },
+        {
+          id: 11,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Fiona',
+        },
+      ],
+      messagesData: [
+        {
+          id: 1,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Dima',
+          message: 'Hey,how are you?',
+        },
+        {
+          id: 2,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Alina',
+          message: 'I am fine.Are you ready to fun?',
+        },
+        {
+          id: 3,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Ben',
+          message: 'Yes I am!',
+        },
+        {
+          id: 4,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Vitaliy',
+          message: 'Coco Loko! Alo Alo...',
+        },
+        {
+          id: 5,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Dima',
+          message: 'Hey,how are you?',
+        },
+        {
+          id: 6,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Alina',
+          message: 'I am fine.Are you ready to fun?',
+        },
+        {
+          id: 7,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Ben',
+          message: 'Yes I am!',
+        },
+        {
+          id: 8,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Vitaliy',
+          message: 'Coco Loko! Alo Alo...',
+        },
+      ],
+      newMessageText: '',
+    },
+    sideBar: {
+      friendsData: [
+        {
+          id: 1,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Dima',
+        },
+        {
+          id: 2,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Alina',
+        },
+        {
+          id: 3,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Ben',
+        },
+        {
+          id: 4,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Vitaliy',
+        },
+        {
+          id: 5,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Olga',
+        },
+        {
+          id: 6,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Kate',
+        },
+        {
+          id: 7,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Caren',
+        },
+        {
+          id: 8,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Kostya',
+        },
+        {
+          id: 9,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Inna',
+        },
+        {
+          id: 10,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Nick',
+        },
+        {
+          id: 11,
+          avatar:
+            'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+          name: 'Fiona',
+        },
+      ],
+    },
+  },
+
+  getState() {
+    return this._state;
+  },
+
+  addPost() {
+    let randomId = Math.floor(Math.random() * 10) + 1;
+    let newPost = {
+      id: randomId,
+      name: 'userName',
+      avatar: 'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+      message: this._state.profilePage.newPostText,
+      likeCounts: 0,
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._subscriber(this._state);
+  },
+
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._subscriber(this._state);
+  },
+
+  addMessage() {
+    let randomId = Math.floor(Math.random() * 10) + 1;
+    let newMessage = {
+      id: randomId,
+      avatar: 'https://freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg',
+      name: 'userName',
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._state.dialogsPage.newMessageText = '';
+    this._subscriber(this._state);
+  },
+
+  updateNewMessageText(newMessage) {
+    this._state.dialogsPage.newMessageText = newMessage;
+    this._subscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._subscriber = observer;
+  },
+};
+/////////////////////////////////////////////////////////////////////
+let rerenderTree = () => {};
 
 let state = {
   profilePage: {
@@ -205,7 +495,7 @@ let state = {
   },
 };
 
-export let addPost = () => {
+export const addPost = () => {
   let randomId = Math.floor(Math.random() * 10) + 1;
   let newPost = {
     id: randomId,
@@ -219,14 +509,14 @@ export let addPost = () => {
   rerenderTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderTree(state);
 };
 
 window.state = state;
 
-export let addMessage = () => {
+export const addMessage = () => {
   let randomId = Math.floor(Math.random() * 10) + 1;
   let newMessage = {
     id: randomId,
@@ -239,9 +529,13 @@ export let addMessage = () => {
   rerenderTree(state);
 };
 
-export let updateNewMessageText = (newMessage) => {
+export const updateNewMessageText = (newMessage) => {
   state.dialogsPage.newMessageText = newMessage;
   rerenderTree(state);
+};
+
+export const subscribe = (observer) => {
+  rerenderTree = observer;
 };
 
 export default state;
